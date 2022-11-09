02733443ded0c6c19e7f5c702f3b5f60c4cc9dc7
Morgann Thain <morgann.thain@gmail.com>
2022-11-09 14:27:16 -0500

    crit descriptions updates

--- src/assets/content/crit.jsx
+++ src/assets/content/crit.jsx
@@ -14,13 +14,37 @@ export const crit1 = {
 };
 
 export const crit2 = {
-  leftSubtitle: "“Create Account & Profile is too long, cluttered, and boring!”",
-  leftText: " We added progress bars, colloquial and engaging language, and separated each question in account creation onto their own screen",
+  leftSubtitle: "“I don't understand why I can't keep messaging somebody after I react to their profile…”",
+  leftText: "We added a new Reaction sent! screen!",
   img: {
     // TODO: add in + resize image
     src: placeholder,
     alt: "Delight logo",
-    // we can resize images using this id tag in App.css dd
+    // we can resize images using this id tag in App.css
+    id: "placeholder"
+  }
+};
+
+export const crit3 = {
+  leftSubtitle: "“Preferences is not important enough for the navbar”",
+  leftText: "We made a new Settings page that includes Preferences and other important options and information",
+  img: {
+    // TODO: add in + resize image
+    src: placeholder,
+    alt: "Delight logo",
+    // we can resize images using this id tag in App.css
+    id: "placeholder"
+  }
+};
+
+export const crit4 = {
+  leftSubtitle: "“I don't understand what the pink colors mean or when I'm actually matched”",
+  leftText: "The app always has a strong color theme: blue when unmatched, pink when matched. Matching also permanently changes the heart profile-viewing button to a messaging button for your new match!",
+  img: {
+    // TODO: add in + resize image
+    src: placeholder,
+    alt: "Delight logo",
+    // we can resize images using this id tag in App.css
     id: "placeholder"
   }
 };
